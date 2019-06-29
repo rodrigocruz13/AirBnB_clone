684efc55237bb69921149562d3f8892da772a7fc
miguelfajardoc <775@holbertonschool.com>
2019-06-29 21:29:11 +0000

    [fix] final format of updated and created attributes

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,7 @@ class BaseModel():
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -29,19 +28,16 @@ class BaseModel():
     def save(self):
         """
         """
-        self.updated_at = str(datetime.datetime.now())
+        self.updated_at = datetime.datetime.now()
 
 
     def to_dict(self):
-        """Class method
-        """
-        """d = {'id' : self.id, 'created_at' : self.created_at,
-             'updated at' : self.updated_at}"""
         dic = {}
         olddic = self.__dict__
         for key in olddic:
-            if key == self.created_at or key == self.updated_at:
+            if key == 'created_at' or key == 'updated_at':
                 dic[key] = olddic[key].isoformat()
-            dic[key] = olddic[key]
+            else:
+                dic[key] = olddic[key]
         dic['__class__'] = self.__class__.__name__
         return dic
