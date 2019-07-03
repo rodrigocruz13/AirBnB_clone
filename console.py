5ae9e33fcebfb517467d8fc73b119310be9f9739
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-07-03 15:06:39 -0500

    [Add]. Task 10. Added functionality to work with other classes

--- console.py
+++ console.py
@@ -2,8 +2,15 @@
 
 import cmd
 import json
+
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 from models import storage
 
 cls_lst = ["BaseModel", "User", "State", "Amenity", "Place", "Review", "City"]
