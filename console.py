b5ae2e26d9194b682773bd2c60d5826208ac6bdb
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-07-03 14:35:43 -0500

    [Fix]. Fixed output

--- console.py
+++ console.py
@@ -45,8 +45,7 @@ class HBNBCommand(cmd.Cmd):
         """
 
         args = args.split(" ")
-
-        if args[0] is None:
+        if args[0] == '':
             print("** class name missing **")
         else:
             new_class_instance = None
@@ -55,8 +54,8 @@ class HBNBCommand(cmd.Cmd):
                 new_class_instance.save()
                 print(new_class_instance.id)
 
-        if new_class_instance is None:
-            print("** class doesn't exist **")
+            if new_class_instance is None:
+                print("** class doesn't exist **")
 
     def do_all(self, args):
         """
@@ -109,7 +108,7 @@ class HBNBCommand(cmd.Cmd):
 
         args = args.split(" ")
 
-        if (len(args) == 0):
+        if (args[0] == ''):
             print("** class name missing **")
         else:
             if (args[0] not in cls_lst):
@@ -195,31 +194,37 @@ class HBNBCommand(cmd.Cmd):
         args = args.split(" ")
         if (args[0] == ""):
             print("** class name missing **")
-        else:
-            if (args[0] not in cls_lst):
-                print("** class doesn't exist **")
-            else:
-                if (len(args) == 1 and args[0] in cls_lst):
-                    print("** instance id missing **")
-                else:
-                    key = args[0] + "." + args[1]
-                    dict_insts = storage.all()
-                    if (key not in dict_insts):
-                        print("** no instance found **")
-                    else:
-                        if (len(args) == 2):
-                            print("** attribute name missing **")
-                        else:
-                            s1 = "updated_at"
-                            s2 = "created_at"
-                            if (args[2] != s1 and args[2] != s2):
-                                new_val = ' '.join(args[3:])
+            return
 
-                                if (len(args[3:]) > 1):
-                                    new_val = new_val[1: -1]
-                                if (args[2] in dir(dict_insts[key])):
-                                    setattr(dict_insts[key], args[2], new_val)
-                                    storage.save()
+        if (args[0] not in cls_lst):
+            print("** class doesn't exist **")
+            return
+
+        if (len(args) == 1 and args[0] in cls_lst):
+            print("** instance id missing **")
+            return
+
+        key = args[0] + "." + args[1]
+        dict_insts = storage.all()
+        if (key not in dict_insts):
+            print("** no instance found **")
+            return
+
+        if (len(args) == 2):
+            print("** attribute name missing **")
+            return
+
+        if (args[2] != "updated_at" and args[2] != "created_at"):
+            if (len(args) < 4):
+                print("** value missing **")
+                return
+            else:
+                new_val = ' '.join(args[3:])
+                if (len(args[3:]) > 1):
+                    new_val = new_val[1: -1]
+
+                setattr(dict_insts[key], args[2], new_val)
+                storage.save()
 
     """ II. helper functions: """
     def emptyline(self):
